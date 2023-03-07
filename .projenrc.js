cabdd1f1948b667f48f1b30fc339420535ff4d47
Marnix Dessing <m.dessing@nijmegen.nl>
2023-03-07 15:10:06 +0100

    feat: use nijmegen projen project type

--- .projenrc.js
+++ .projenrc.js
@@ -1,27 +1,28 @@
-const { typescript } = require('projen');
-const { NpmAccess } = require('projen/lib/javascript');
+const { GemeenteNijmegenTsPackage } = require('@gemeentenijmegen/projen-project-type');
 
 const projectName = '@gemeentenijmegen/apiclient';
 
-const project = new typescript.TypeScriptProject({
+const project = new GemeenteNijmegenTsPackage({
   defaultReleaseBranch: 'main',
   name: projectName,
   defaultReleaseBranch: 'main',
-  license: 'EUPL-1.2',
-  release: true,
-  releaseToNpm: true,
-  npmAccess: NpmAccess.PUBLIC,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'], // No acceptance branche
+    },
+  },
   deps: [
     '@aws-sdk/client-secrets-manager',
     '@aws-sdk/client-ssm',
     'axios',
-  ], /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  ],
   devDeps: [
     'dotenv',
     'axios-mock-adapter',
     'jest-aws-client-mock',
-  ], /* Build dependencies for this module. */
-  packageName: projectName, /* The "name" in package.json. */
+    '@gemeentenijmegen/projen-project-type',
+  ],
+  packageName: projectName, 
+  enableAutoMergeDependencies: false, // No acceptance branche
 });
 project.synth();
