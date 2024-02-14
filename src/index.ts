c64b923b2f2c8313b33e924addd73ee58377c1b4
Marnix Dessing <m.dessing@nijmegen.nl>
2024-02-14 15:08:06 +0100

    feat: throw epecific error on timeout

--- src/index.ts
+++ src/index.ts
@@ -152,6 +152,9 @@ export class ApiClient {
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
         console.error(error?.code);
+        if(error?.code === 'ECONNABORTED'){
+          throw new Error('Het ophalen van gegevens duurde te lang.');
+        }
       } else {
         // Something happened in setting up the request that triggered an Error
         console.error(error.message);
